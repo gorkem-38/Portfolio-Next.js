87d2cf7745d441e97277ccb2a5f712499aa55f14
Tumer Gorkem <tumergorkem@Macbouk.local>
2022-10-26 13:33:47 +0200

    front-end part finish

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,6 +1,7 @@
 import Header from "./header";
 import SectionName from "./sectionName";
 import SectionLink from "./sectionLink";
+import SectionWork from "./sectionWork";
 
 const Layout = () => {
     return (
@@ -8,6 +9,7 @@ const Layout = () => {
             <Header />
             <SectionName />
             <SectionLink />
+            <SectionWork />
         </>
     )
 }
--- src/components/layout/sectionWork.jsx
+++ src/components/layout/sectionWork.jsx
@@ -1,7 +1,17 @@
+let styles = {
+    sectionWork: "h-screen"
+}
+
 const SectionWork = () => {
     return (
         <>
-            
+            <section className={styles.sectionWork}>
+                <div>
+
+                </div>
+            </section>
         </>
     )
 }
+
+export default SectionWork;
