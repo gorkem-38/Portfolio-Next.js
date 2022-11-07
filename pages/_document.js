543ebcb0f21127399a45cf2b39116e8a7513692b
Tumer Gorkem <tumergorkem@Macbouk.local>
2022-11-07 14:26:16 +0100

    download

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,6 @@ export default function Document() {
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" />
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" />
                 <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
-                <script src="./TW-ELEMENTS-PATH/dist/js/index.min.js"></script>
             </Head>
             <body>
                 <Main />
--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -5,25 +5,27 @@ import {
     CardFooter,
     Typography,
   } from "@material-tailwind/react";
-
-  import Link from 'next/link'
+import Link from 'next/link'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
   let styles = {
-    sectionWork: "pt-36 ",
-    divCard: "h-[39rem] bg-[#6e07f3] text-white rounded-none",
+    sectionWork: "pt-36",
+    divCard: "h-[35rem] bg-[#6e07f3] text-white rounded-none",
     cardHeader: "flex flex-col items-center justify-evenly text-white relative h-80 py-4 mx-7 bg-[#141c3a]",
     typoHeader: "text-xl font-arimo font-bold",
     typoHeader2: "text-base text-center font-arimo px-9 pb-6",
     linkHeader: "border border-[#5be9b9] p-3 rounded-full hover:bg-[#5be9b9] duration-300",
     cardBody: "text-center",
     typoBody: "text-xl pt-4 font-arimo font-bold",
-    divBody: "flex justify-center font-arimo"
+    divBody: "flex justify-evenly items-end h-20 font-arimo px-10",
+    hoverIcon: "hover:text-[#5be9b9] transition duration-[400ms]"
 }
 
 const Footer = () => {
     return (
         <>
-            <section className={styles.sectionWork}>
+            <section className={styles.sectionWork} >
                 <Card className={styles.divCard}>
                     <CardHeader className={styles.cardHeader}>
                         <Typography className={styles.typoHeader}>Démarrer un projet</Typography>
@@ -36,15 +38,15 @@ const Footer = () => {
                     <CardBody className={styles.cardBody}>
                         <Typography className={styles.typoBody}>Développeur web front-end</Typography>
                         <div className={styles.divBody}>
-                            {/* <Link href="">
-
+                            <Link href="#">
+                                <a><GitHubIcon fontSize="large" className={styles.hoverIcon} /></a>
                             </Link>
                             <Link href="">
-
+                                <a><LinkedInIcon fontSize="large" className={styles.hoverIcon} /></a>
                             </Link>
-                            <Link href="">
-
-                            </Link> */}
+                        </div>
+                        <div className="font-">
+                            <p>Coder par moi © Tumer Gorkem</p>
                         </div>
                     </CardBody>
                 </Card>
