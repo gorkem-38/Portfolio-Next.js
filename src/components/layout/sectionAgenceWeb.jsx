92767f38f048173f73c68b9b063440d2e9035727
Tumer Gorkem <tumergorkem@Macbouk.local>
2022-11-22 09:35:16 +0100

    updte

--- src/components/layout/sectionAgenceWeb.jsx
+++ src/components/layout/sectionAgenceWeb.jsx
@@ -1,48 +1,58 @@
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Typography,
-  } from "@material-tailwind/react";
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+} from "@material-tailwind/react";
 
-import Image from 'next/image';
-import metacode from '@src/assets/img/metacode.png'
-import groupProjet from '@src/assets/img/groupProject.jpg'
+import Image from "next/image";
+import metacode from "@src/assets/img/metacode.png";
+import groupProjet from "@src/assets/img/groupProject.jpg";
+import Link from "next/link";
 
 let styles = {
-    sectionWork: "pt-10 lg:grid lg:grid-cols-2 lg:my-20 lg:hover:bg-[#5be9b9] lg:duration-300 lg:transition lg:ease-in-out lg:hover:-translate-y-3",
-    divCard: "h-80 shadow-2xl mx-8 bg-[#6e07f3] text-white md:mx-20 lg:my-12",
-    cardHeader: "relative h-[45%] mx-6 md:mx-16",
-    icon: "hidden lg:block lg:ml-56 lg:mt-28 lg:h-auto lg:w-72 lg:animate-bounce lg:opacity-80",
-    cardBody: "text-center",
-    cardTypo: "text-xl pb-4 font-arimo font-bold",
-    cardTypo2: "font-martel text-sm"
-}
+  sectionWork:
+    "pt-10 lg:grid lg:grid-cols-2 lg:my-20 lg:hover:bg-[#5be9b9] lg:duration-300 lg:transition lg:ease-in-out lg:hover:-translate-y-3",
+  divCard: "h-80 shadow-2xl mx-8 bg-[#6e07f3] text-white md:mx-20 lg:my-12",
+  cardHeader: "relative h-[45%] mx-6 md:mx-16",
+  icon: "hidden lg:block lg:ml-56 lg:mt-28 lg:h-auto lg:w-72 lg:animate-bounce lg:opacity-80",
+  cardBody: "text-center",
+  cardTypo: "text-xl pb-4 font-arimo font-bold",
+  cardTypo2: "font-martel text-sm",
+};
 
 const SectionAgenceWeb = () => {
-    return (
-        <>
-            <section 
-            className={styles.sectionWork}
-            data-aos-duration="1000"
-            data-aos="zoom-in-up"
-            data-aos-once="false">
-                <Card className={styles.divCard}>
-                    <CardHeader className={styles.cardHeader}>
-                        <Image src={metacode} alt="metacode" />
-                    </CardHeader>
-                    <CardBody className={styles.cardBody}>
-                        <Typography className={styles.cardTypo}>Agence web</Typography>
-                        <Typography className={styles.cardTypo2}>1er projet collectif dans un environnement Webpack, création d&apos;une agence web. Hébergement du site sur Netlify.</Typography>
-                    </CardBody>
-                </Card>
-                <div className={styles.icon}>
-                    <Image src={groupProjet} className="rounded-full" />
-                </div>
-            </section>
-        </>
-    )
-}
+  return (
+    <>
+      <section
+        className={styles.sectionWork}
+        data-aos-duration="1000"
+        data-aos="zoom-in-up"
+        data-aos-once="false"
+      >
+        <Card className={styles.divCard}>
+          <CardHeader className={styles.cardHeader}>
+            <Link href="https://meta-code.netlify.app/" target="_blank">
+              <a>
+                <Image src={metacode} alt="metacode" />
+              </a>
+            </Link>
+          </CardHeader>
+          <CardBody className={styles.cardBody}>
+            <Typography className={styles.cardTypo}>Agence web</Typography>
+            <Typography className={styles.cardTypo2}>
+              1er projet collectif dans un environnement Webpack, création
+              d&apos;une agence web. Hébergement du site sur Netlify.
+            </Typography>
+          </CardBody>
+        </Card>
+        <div className={styles.icon}>
+          <Image src={groupProjet} className="rounded-full" />
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default SectionAgenceWeb;
--- src/components/layout/sectionLandingPage.jsx
+++ src/components/layout/sectionLandingPage.jsx
@@ -1,62 +1,75 @@
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Typography,
-  } from "@material-tailwind/react";
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+} from "@material-tailwind/react";
 
-import Image from 'next/image';
-import landing from '@src/assets/img/landing.png'
-import landingIcon from '@src/assets/img/landingIcon.jpg'
+import Image from "next/image";
+import landing from "@src/assets/img/landing.png";
+import landingIcon from "@src/assets/img/landingIcon.jpg";
+import Link from "next/link";
 
 let styles = {
-    sectionWork: "h-max pb-10",
-    divParagraph: "flex flex-col items-center pt-20",
-    h2: "font-arimo text-xl underline font-black pb-4 md:text-2xl",
-    gcols2: "hidd lg:grid lg:grid-cols-2 lg:mt-14 lg:hover:bg-[#5be9b9] lg:duration-300 lg:transition lg:ease-in-out lg:hover:-translate-y-3",
-    divP: "text-center px-10 font-arimo md:text-lg",
-    wh: "hidden lg:block lg:animate-bounce lg:ml-60 lg:mt-40 lg:h-auto lg:w-64 lg:rounded-full lg:opacity-80",
-    divLandingPage: "pt-20",
-    divCard: "h-80 shadow-2xl mx-8 bg-[#6e07f3] text-white md:mx-16 lg:mb-12",
-    cardHeader: "relative h-[45%] mx-6 md:mx-20",
-    cardBody: "text-center",
-    cardTypo: "text-xl font-arimo font-bold pb-4",
-    cardTypo2: "font-martel text-sm" 
-}
+  sectionWork: "h-max pb-10",
+  divParagraph: "flex flex-col items-center pt-20",
+  h2: "font-arimo text-xl underline font-black pb-4 md:text-2xl",
+  gcols2:
+    "hidd lg:grid lg:grid-cols-2 lg:mt-14 lg:hover:bg-[#5be9b9] lg:duration-300 lg:transition lg:ease-in-out lg:hover:-translate-y-3",
+  divP: "text-center px-10 font-arimo md:text-lg",
+  wh: "hidden lg:block lg:animate-bounce lg:ml-60 lg:mt-40 lg:h-auto lg:w-64 lg:rounded-full lg:opacity-80",
+  divLandingPage: "pt-20",
+  divCard: "h-80 shadow-2xl mx-8 bg-[#6e07f3] text-white md:mx-16 lg:mb-12",
+  cardHeader: "relative h-[45%] mx-6 md:mx-20",
+  cardBody: "text-center",
+  cardTypo: "text-xl font-arimo font-bold pb-4",
+  cardTypo2: "font-martel text-sm",
+};
 
 const SectionLandingPage = () => {
-    return (
-        <>
-            <section 
-            className={styles.sectionWork} 
-            data-aos-duration="1000"
-            data-aos="zoom-in-up"
-            data-aos-once="false"
-            >
-                <div className={styles.divParagraph}>
-                    <h2 className={styles.h2}>Mes projets récents</h2>
-                    <p className={styles.divP}>Voici quelques projets sur lesquels j&apos;ai travaillé.</p>
-                </div>
-                <section className={styles.gcols2}>
-                    <div className={styles.wh}>
-                        <Image src={landingIcon} className="rounded-full" />
-                    </div>
-                    <div className={styles.divLandingPage}>
-                        <Card className={styles.divCard}>
-                            <CardHeader className={styles.cardHeader}>
-                                <Image src={landing} alt="landing" />
-                            </CardHeader>
-                            <CardBody className={styles.cardBody}>
-                                <Typography className={styles.cardTypo}>Landing Page</Typography>
-                                <Typography className={styles.cardTypo2}>1er projet individuel, maquettage et réalisation du site en vanilla. Hébergement du site a l&apos;aide de FileZilla</Typography>
-                            </CardBody>
-                        </Card>
-                    </div>
-                </section>
-            </section>
-        </>
-    )
-}
+  return (
+    <>
+      <section
+        className={styles.sectionWork}
+        data-aos-duration="1000"
+        data-aos="zoom-in-up"
+        data-aos-once="false"
+      >
+        <div className={styles.divParagraph}>
+          <h2 className={styles.h2}>Mes projets récents</h2>
+          <p className={styles.divP}>
+            Voici quelques projets sur lesquels j&apos;ai travaillé.
+          </p>
+        </div>
+        <section className={styles.gcols2}>
+          <div className={styles.wh}>
+            <Image src={landingIcon} className="rounded-full" />
+          </div>
+          <div className={styles.divLandingPage}>
+            <Card className={styles.divCard}>
+                  <CardHeader className={styles.cardHeader}>
+              <Link href="https://l-page.netlify.app/">
+                <a>
+                    <Image src={landing} alt="landing" />
+                </a>
+              </Link>
+                  </CardHeader>
+              <CardBody className={styles.cardBody}>
+                <Typography className={styles.cardTypo}>
+                  Landing Page
+                </Typography>
+                <Typography className={styles.cardTypo2}>
+                  1er projet individuel, maquettage et réalisation du site en
+                  vanilla. Hébergement du site a l&apos;aide de FileZilla
+                </Typography>
+              </CardBody>
+            </Card>
+          </div>
+        </section>
+      </section>
+    </>
+  );
+};
 
 export default SectionLandingPage;
